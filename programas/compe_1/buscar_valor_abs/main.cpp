be9e7b7992feedcce5c2e4a7f6ec30abac459c26
Bal3x <bchavez_@outlook.com>
2021-11-25 09:03:28 -0500

    Add files to compe_1

---
+++ programas/compe_1/buscar_valor_abs/main.cpp
@@ -0,0 +1,29 @@
+/*
+ * Buscar el valor absoluto de un # que se solicita al usuario
+ * Por: Bryan Chavez
+ * 
+ * 3 soluciones
+ *  1. compe0, sin usar funciones
+ *  2. usando la funcion fabs de la libreria matematica
+ *  3. Crear mi propia funcion fabs (myfabs)
+ * 
+ * fecha: 11/22/2021
+ * */
+
+#include <iostream>
+using namespace std;
+
+int main(){         // caso base sin usar funciones. Para usar como referencia.
+    
+    int i = 3;
+    
+    cout << ++i << '\n';
+    cout << i << '\n';
+    
+    
+    return 0;
+    
+}
+
+    
+
