281a6c51b4345f950847a2ab7daa1f470b35dd3b
Bal3x <bchavez_@outlook.com>
2021-11-30 11:16:35 -0500

    Add files

--- programas/compe_1/buscar_valor_abs3/buscar_valor_abs3.mk
+++ programas/compe_1/buscar_valor_abs3/buscar_valor_abs3.mk
@@ -3,10 +3,10 @@
 ## any manual changes will be erased      
 ##
 ## Debug
-ProjectName            :=buscar_valor_abs
+ProjectName            :=buscar_valor_abs3
 ConfigurationName      :=Debug
 WorkspacePath          :=/home/bal3x/clases/compe_1/programas/compe_1
-ProjectPath            :=/home/bal3x/clases/compe_1/programas/compe_1/buscar_valor_abs
+ProjectPath            :=/home/bal3x/clases/compe_1/programas/compe_1/buscar_valor_abs3
 IntermediateDirectory  :=$(ConfigurationName)
 OutDir                 := $(IntermediateDirectory)
 CurrentFileName        :=
@@ -32,7 +32,7 @@ Preprocessors          :=
 ObjectSwitch           :=-o 
 ArchiveOutputSwitch    := 
 PreprocessOnlySwitch   :=-E
-ObjectsFileList        :="buscar_valor_abs.txt"
+ObjectsFileList        :="buscar_valor_abs3.txt"
 PCHCompileFlags        :=
 MakeDirCommand         :=mkdir -p
 LinkOptions            :=  -static
@@ -93,7 +93,7 @@ PreBuild:
 ##
 $(IntermediateDirectory)/main.cpp$(ObjectSuffix): main.cpp
 	@$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) -MG -MP -MT$(IntermediateDirectory)/main.cpp$(ObjectSuffix) -MF$(IntermediateDirectory)/main.cpp$(DependSuffix) -MM main.cpp
-	$(CXX) $(IncludePCH) $(SourceSwitch) "/home/bal3x/clases/compe_1/programas/compe_1/buscar_valor_abs/main.cpp" $(CXXFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/main.cpp$(ObjectSuffix) $(IncludePath)
+	$(CXX) $(IncludePCH) $(SourceSwitch) "/home/bal3x/clases/compe_1/programas/compe_1/buscar_valor_abs3/main.cpp" $(CXXFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/main.cpp$(ObjectSuffix) $(IncludePath)
 $(IntermediateDirectory)/main.cpp$(PreprocessSuffix): main.cpp
 	$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) $(PreprocessOnlySwitch) $(OutputSwitch) $(IntermediateDirectory)/main.cpp$(PreprocessSuffix) main.cpp
 
--- programas/compe_1/buscar_valor_abs3/main.cpp
+++ programas/compe_1/buscar_valor_abs3/main.cpp
@@ -13,37 +13,36 @@
 #include <iostream>
 using namespace std;
 
-double myfabs();
-
-
+double myfabs(double);        //Prototipo y lleva ; (hacer un adelanto a la funcion para no 
+                                //tener problema con el orden fisico en compilacion
+        
 int main(){         // caso creando mi propia funcion fabs (myfabs)
-    
+    //variables
     double n, va;
+    // llamado a usuario a entrar un numero, activa el teclado
+    cout << "Entre un numero para obtener su valor absoluto: ";
+    cin >> n;                       // usuario entra numero y es guarda el valor
+    // llamado de la funcion
+    va = myfabs(n);     
     
-    cout << "Entre un numero: ";
-    cin >> n;
-    
-    va = myfabs(n);
-    
-    cout << '|' << n << "| = " << va << "\n\n"
+    // Imprimir el valor absoluto 
+    cout << '|' << n << "| = " << va << "\n\n";
 
     return 0;
     
 }
 
-    
-    {
+double myfabs(double n)     // encabezado de la funcion
+{
+        // variables
         double va; 
-        
-            if ( n < 0.0){
-                va = -n;
-            }
+        // Codigo de la funcion
+        if ( n < 0.0)
+            va = -n;
+        else
+            va = n;
             
-            else{
-                va = n;
-            }
-        
-        return va;
-        
-        
-        }
+        return va;      // retorna valor a la variable anterior de main
+    }
+    
+    
