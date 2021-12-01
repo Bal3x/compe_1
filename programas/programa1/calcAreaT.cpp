0157d45835ec3ac85f1b1c1063d0665cfb82a7e3
bal3x <bchavez_@outlook.com>
2021-12-01 12:50:49 -0500

    Add files from comp class

---
+++ programas/programa1/calcAreaT.cpp
@@ -0,0 +1,46 @@
+/*
+Programa para calcular el area de un triangulo
+usando una funcion que no sea main\
+Creado por : Bryan Chavez
+*/
+
+#include <iostream>
+using namespace std;
+
+double calcAreaT(double , double ); //Prototipo
+
+int main(){
+
+    double b, h, A;     //variables que se van a utilizar
+
+    // pedir la base y la altura del triangulo al usuario
+    cout << "Entre la base del triangulo: " << endl;
+    cin >> b;
+
+    while(b <= 0.0){
+        cout << "Base invalida. Re-entre un valor positivo." << endl;
+        cin >> b;
+    }
+    
+    cout << "Entre la altura del triangulo: " << endl;
+    cin >> h; 
+
+    while(h <= 0.0){
+        cout << "Base invalida. Re-entre un valor positivo." << endl;
+        cin >> h;
+    }
+
+    // Calcular el area del triangulo - se llama una funcion que es la que hace el trabajo
+    A = calcAreaT(b,h);
+
+    // Imprimir el area
+    cout << "Area del triangulo = " << A << endl; 
+
+    return 0;
+}
+
+double calcAreaT(double b, double h){
+    
+    return 1.0/2.0 * b * h; // retorna el calculo de la funcion. en vez de crear el 
+                            //programa completo se puede hacer un return directo.
+}
---
+++ programas/programa1/radioCirculo.cpp
@@ -0,0 +1,34 @@
+/*
+Una funcion que solicite al usuario el radio de un circulo.
+
+Creado por : Bryan Chavez
+*/
+
+#include <iostream>
+using namespace std;
+double radioCirculo();
+
+
+int main(){
+
+    radioCirculo();
+
+    return 0;
+}
+
+double radioCirculo(){
+
+    // definir variables locales
+    double r;       
+    
+    //pedir el radio del circulo
+    cout << "Entre el radio del circulo: " << endl;
+    cin >> r;           
+    //validar el radio del circulo
+    while(r <= 0.0){
+        cout << "Radio invalido. Intente de nuevo con un numero positivo." << endl;
+        cin >> r;
+    }
+
+    return r;
+}
