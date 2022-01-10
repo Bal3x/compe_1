6141bee2b09f8f1f933cb66a90bad10aa1acf7d9
bal3x <bchavez_@outlook.com>
2022-01-09 20:04:32 -0500

    Modifica codigo para que incluya un loop para incluir codigo que pide al usuario nota del estudiante

--- programas/practica/notas_arreglos.cpp
+++ programas/practica/notas_arreglos.cpp
@@ -12,16 +12,27 @@ using namespace std;
 int main(){
 // variables
     const int as = 40;
+    double ex1[as], ex2[as];
     int n, i;
 //codigo que solicita al usuario cuantos estudiantes hay
-    cout << "Cuantos estudiantes hay en su clase? " << endl;
+    cout << "Cuantos estudiantes hay en su clase?" << endl;
     cin >> n;
-    while(n < 1 || n > as){
-        cout << "Valor invalido, por favor intente nuevamente con un valor entre " << 1 << " y " << as << endl;
+    while (n < 1 || n > as){
+        cout << "Valor invalido, por favor intente nuevamente con un valor entre 1 y " << as << endl;
         cin >> n;
     } 
 //codigo que solicita al usuario las notas de los 4 examenes
-    // for (i = 0; i < n)
+    for (i = 0; i < n; i++){
+// codigo que pida al usuario la nota del primer examen
+        cout << "Entre la nota del primer examen para el estudiante " << i + 1 << " : " << endl;
+        cin >> ex1[i];
+        while(ex1[i] < 0.0 || ex1[i] > 100.0){
+            cout << "Valor invalido, por favor entre un valor entre 0 - 100." << endl;
+            cin >> ex1[i];
+        }
+//codigo que pida al usuario la nota del segundo examen
+    
+    }
     return 0;
 
 }
