4951e3542d22ed729057f1af54beb6971c089ba4
bal3x <bchavez_@outlook.com>
2022-01-23 22:19:33 -0500

    Add funciones al programa de practica

--- programas/practica/programa7_2.cpp
+++ programas/practica/programa7_2.cpp
@@ -3,13 +3,16 @@
 
 #include <iostream>
 using namespace std;
+
 int numEmpleados();
 void solicitarHoras(int [], const int);
 float promedioHoras(const int [], const int);
+void horasEmpleados(const int [], const int);
+void imprimePromedio(int, const int);
 
 int main(){
 
-    int n = 0;
+    int n = 30;
     int horasTrabajadas[n] = {0};
     float promedio = 0.0;
     // Funcion que solicite al usuario el numero de empleados
@@ -18,8 +21,10 @@ int main(){
     solicitarHoras(horasTrabajadas, n);
     // funcion que calcula el promedio de las horas trabajadas
     promedio = promedioHoras(horasTrabajadas, n);
+    // funcion que imprima las horas trabajas por cada empleado
+    horasEmpleados(horasTrabajadas, n);
     // imprimir el promedio de horas de los empleados
-    cout << "El promedio de horas de los " << n << " empleados es " << promedio << endl;
+    imprimePromedio(promedio, n);
 
     return 0;
 }
@@ -27,8 +32,8 @@ int numEmpleados(){
     int n;
     cout << "Entre el numero de empleados: " << endl;
     cin >> n;
-    while (n <= 0){
-        cout << "Numero invalido, intente nuevamente con un numero positivo.";
+    while (n <= 0 || n > 30){
+        cout << "Numero invalido, intente nuevamente con un numero positivo entre 1 - 30.";
         cin >> n;
     }
     return n;
@@ -49,4 +54,12 @@ float promedioHoras(const int a[], const int n){
         sum += a[i];
     }
     return sum / n;
+}
+void horasEmpleados(const int a[], const int n){
+    for(int i = 0; i < n; i++){
+        cout << "Las horas trabajadas por empleado " << i + 1 << " son: " << a[i] << endl;
+    }
+}
+void imprimePromedio(int promedio, const int n){
+        cout << "El promedio de horas de los " << n << " empleados es " << promedio << endl;
 }
