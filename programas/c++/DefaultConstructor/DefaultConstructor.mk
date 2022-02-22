ef5c6df0e8f139e37c3c97ccd7eccb23f2f619b1
Bal3x <bchavez_@outlook.com>
2022-02-22 16:06:04 -0500

    Add files to C++ class

--- programas/c++/DefaultConstructor/DefaultConstructor.mk
+++ programas/c++/DefaultConstructor/DefaultConstructor.mk
@@ -13,7 +13,7 @@ CurrentFileName        :=
 CurrentFilePath        :=
 CurrentFileFullPath    :=
 User                   :=Bal3x
-Date                   :=21/02/22
+Date                   :=22/02/22
 CodeLitePath           :=/home/bal3x/.codelite
 LinkerName             :=/usr/bin/g++
 SharedObjectLinkerName :=/usr/bin/g++ -shared -fPIC
@@ -60,7 +60,7 @@ AS       := /usr/bin/as
 ## User defined environment variables
 ##
 CodeLiteDir:=/usr/share/codelite
-Objects0=$(IntermediateDirectory)/main.cpp$(ObjectSuffix) 
+Objects0=$(IntermediateDirectory)/Dog.cpp$(ObjectSuffix) $(IntermediateDirectory)/main.cpp$(ObjectSuffix) 
 
 
 
@@ -91,6 +91,12 @@ PreBuild:
 ##
 ## Objects
 ##
+$(IntermediateDirectory)/Dog.cpp$(ObjectSuffix): Dog.cpp
+	@$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) -MG -MP -MT$(IntermediateDirectory)/Dog.cpp$(ObjectSuffix) -MF$(IntermediateDirectory)/Dog.cpp$(DependSuffix) -MM Dog.cpp
+	$(CXX) $(IncludePCH) $(SourceSwitch) "/home/bal3x/clases/compe_1/programas/c++/DefaultConstructor/Dog.cpp" $(CXXFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/Dog.cpp$(ObjectSuffix) $(IncludePath)
+$(IntermediateDirectory)/Dog.cpp$(PreprocessSuffix): Dog.cpp
+	$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) $(PreprocessOnlySwitch) $(OutputSwitch) $(IntermediateDirectory)/Dog.cpp$(PreprocessSuffix) Dog.cpp
+
 $(IntermediateDirectory)/main.cpp$(ObjectSuffix): main.cpp
 	@$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) -MG -MP -MT$(IntermediateDirectory)/main.cpp$(ObjectSuffix) -MF$(IntermediateDirectory)/main.cpp$(DependSuffix) -MM main.cpp
 	$(CXX) $(IncludePCH) $(SourceSwitch) "/home/bal3x/clases/compe_1/programas/c++/DefaultConstructor/main.cpp" $(CXXFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/main.cpp$(ObjectSuffix) $(IncludePath)
